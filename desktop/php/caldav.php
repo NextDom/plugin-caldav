3bfbec4fe1a9dfb434c212309cb61e616242234a
guenneguezt <thomas@guenneguez.com>
2018-02-18 21:52:48 +0100

    Mise au norme de la gestion d'équipement

--- desktop/php/caldav.php
+++ desktop/php/caldav.php
@@ -22,21 +22,21 @@ $eqLogics = eqLogic::byType('caldav');
     </div>
     
      <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-	  <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
-	   <div class="eqLogicThumbnailContainer">
-		<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-		 <center>
-		  <i class="fa fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
-		</center>
-		<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
-	  </div>
-	  <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
-		<center>
-		  <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
-		</center>
-		<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
-	  </div>
-	</div>
+        <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
+        <div class="eqLogicThumbnailContainer">
+            <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+                <center>
+                    <i class="fa fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
+                </center>
+                <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+            </div>
+            <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+                <center>
+                <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
+                </center>
+                <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
+            </div>
+        </div>
         <legend>{{Mes agendas Caldav}}
         </legend>
 		<div class="eqLogicThumbnailContainer">
@@ -64,85 +64,87 @@ $eqLogics = eqLogic::byType('caldav');
     </div>
 
     <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-        <form class="form-horizontal">
-            <fieldset>
-                <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">{{Nom de l'équipement}}</label>
-                    <div class="col-sm-2">
-                        <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                        <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="Nom de l'équipement caldav"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-2 control-label" >{{Objet parent}}</label>
-                    <div class="col-sm-2">
-                        <select class="eqLogicAttr form-control" data-l1key="object_id">
-                            <option value="">{{Aucun}}</option>
-                            <?php
-                            foreach (object::all() as $object) {
-                                echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
-                </div>
-                <div class="form-group">
-                  <label class="col-sm-2 control-label" ></label>
-					<div class="col-sm-10">
-					<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>Activer</label>
-					<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>Visible</label>
-					</div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-2 control-label">{{URL caldav}}</label>
-                    <div class="col-lg-4">
-                        <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="url"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-2 control-label">{{Compte}}</label>
-                    <div class="col-lg-2">
-                        <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="username"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-2 control-label">{{Password}}</label>
-                    <div class="col-lg-2">
-                        <input type="password" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="password"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-2 control-label">{{Calendrier}}</label>
-                    <div class="col-lg-3">
-                        <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="calendrier" id="CalendarsList">
-                        </select>
-                    </div>
-                </div>
-            </fieldset> 
-        </form>
+        <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+        <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+        <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+            <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+            <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+        </ul>
+        <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+            <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+                <form class="form-horizontal">
+                    <fieldset>
+                        <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">{{Nom de l'équipement}}</label>
+                            <div class="col-sm-2">
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="Nom de l'équipement caldav"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" >{{Objet parent}}</label>
+                            <div class="col-sm-2">
+                                <select class="eqLogicAttr form-control" data-l1key="object_id">
+                                    <option value="">{{Aucun}}</option>
+                                    <?php
+                                    foreach (object::all() as $object) {
+                                        echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                          <label class="col-sm-2 control-label" ></label>
+                            <div class="col-sm-10">
+                            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>Activer</label>
+                            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>Visible</label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-lg-2 control-label">{{URL caldav}}</label>
+                            <div class="col-lg-4">
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="url"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-lg-2 control-label">{{Compte}}</label>
+                            <div class="col-lg-2">
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="username"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-lg-2 control-label">{{Password}}</label>
+                            <div class="col-lg-2">
+                                <input type="password" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="password"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-lg-2 control-label">{{Calendrier}}</label>
+                            <div class="col-lg-3">
+                                <select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="calendrier" id="CalendarsList">
+                                </select>
+                            </div>
+                        </div>
+                    </fieldset> 
+                </form>
+           </div>
+            <div role="tabpanel" class="tab-pane" id="commandtab">
+                <a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une pattern caldav}}</a><br/><br/>
+                <table id="table_cmd" class="table table-bordered table-condensed">
+                    <thead>
+                        <tr>
+                            <th>{{Nom}}</th><th>{{Pattern}}</th><th>{{Valeur par défaut}}</th><th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-        <legend>{{caldav}}</legend>
-        <a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une pattern caldav}}</a><br/><br/>
-        <table id="table_cmd" class="table table-bordered table-condensed">
-            <thead>
-                <tr>
-                    <th>{{Nom}}</th><th>{{Pattern}}</th><th>{{Valeur par défaut}}</th><th></th>
-                </tr>
-            </thead>
-            <tbody>
-
-            </tbody>
-        </table>
-
-        <form class="form-horizontal">
-            <fieldset>
-                <div class="form-actions">
-                    <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-                    <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-                </div>
-            </fieldset>
-        </form>
+                    </tbody>
+                </table>
+           </div>
+        </div>
     </div>
 </div>
 
