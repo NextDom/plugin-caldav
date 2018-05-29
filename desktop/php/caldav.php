35faeaacd1a98d684e06a6894ead1f2590a930a6
ColonelMoutarde <4697568+ColonelMoutarde@users.noreply.github.com>
2018-05-29 15:34:00 +0200

    indentation

--- desktop/php/caldav.php
+++ desktop/php/caldav.php
@@ -1,6 +1,6 @@
 <?php
 if (!isConnect('admin')) {
-    throw new Exception('{{401 - Accès non autorisé}}');
+    throw new \Exception('{{401 - Accès non autorisé}}');
 }
 sendVarToJS('eqType', 'caldav');
 $eqLogics = eqLogic::byType('caldav');
@@ -21,7 +21,7 @@ $eqLogics = eqLogic::byType('caldav');
         </div>
     </div>
 
-     <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
         <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
         <div class="eqLogicThumbnailContainer">
             <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
@@ -32,35 +32,35 @@ $eqLogics = eqLogic::byType('caldav');
             </div>
             <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
                 <center>
-                <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
+                    <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
                 </center>
                 <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
             </div>
         </div>
         <legend>{{Mes agendas Caldav}}
         </legend>
-		<div class="eqLogicThumbnailContainer">
-			  <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-				 <center>
-					<i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
-				</center>
-				<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
-			</div>
-			<?php
-			if (count($eqLogics) == 0) {
-				echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>Vous n'avez encore d'agenda Caldav, cliquez à gauche sur le bouton ajouter un agenda pour commencer</span></center>";
-			} else {
-				foreach ($eqLogics as $eqLogic) {
-					echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
-					echo "<center>";
-					echo '<img src="plugins/caldav/docs/images/caldav_icon.png" height="105" width="95" />';
-					echo "</center>";
-					echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
-					echo '</div>';
-				}
-			}
-			?>
-		</div>
+        <div class="eqLogicThumbnailContainer">
+            <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+                <center>
+                    <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
+                </center>
+                <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+            </div>
+            <?php
+            if (count($eqLogics) == 0) {
+                echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>Vous n'avez encore d'agenda Caldav, cliquez à gauche sur le bouton ajouter un agenda pour commencer</span></center>";
+            } else {
+                foreach ($eqLogics as $eqLogic) {
+                    echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+                    echo "<center>";
+                    echo '<img src="plugins/caldav/docs/images/caldav_icon.png" height="105" width="95" />';
+                    echo "</center>";
+                    echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+                    echo '</div>';
+                }
+            }
+            ?>
+        </div>
     </div>
 
     <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
@@ -97,10 +97,10 @@ $eqLogics = eqLogic::byType('caldav');
                             </div>
                         </div>
                         <div class="form-group">
-                          <label class="col-sm-2 control-label" ></label>
+                            <label class="col-sm-2 control-label" ></label>
                             <div class="col-sm-10">
-                            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>Activer</label>
-                            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>Visible</label>
+                                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>Activer</label>
+                                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>Visible</label>
                             </div>
                         </div>
                         <div class="form-group">
@@ -130,7 +130,7 @@ $eqLogics = eqLogic::byType('caldav');
                         </div>
                     </fieldset>
                 </form>
-           </div>
+            </div>
             <div role="tabpanel" class="tab-pane" id="commandtab">
                 <a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une pattern caldav}}</a><br/><br/>
                 <table id="table_cmd" class="table table-bordered table-condensed">
@@ -143,7 +143,7 @@ $eqLogics = eqLogic::byType('caldav');
 
                     </tbody>
                 </table>
-           </div>
+            </div>
         </div>
     </div>
 </div>
